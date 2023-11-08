db486e247c5917ad9504dbf0c3a4ccf08767d1f6
Grzegorz Wiktorowicz <gwiktoro@camk.edu.pl>
2023-11-08 11:46:26 +0100

    fixes for moccarun and moccatest

--- scripts/moccarun.py
+++ scripts/moccarun.py
@@ -6,7 +6,7 @@ import subprocess
 
 import logging
 logging.basicConfig(format='%(asctime)s|%(levelname)s|%(name)s|%(funcName)s|%(lineno)s|%(message)s')
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
 
 """
 
@@ -21,22 +21,30 @@ sbatch $@ mocca.slurm
 
 """
 
-def moccarun(path='.', mocca_binary_path=None, keep_mocca_binary=False, user_email='gwiktoro@camk.edu.pl', partition=None, dry_run=False, wait=False):
+def moccarun(run_path='.', mocca_binary_path=None, keep_mocca_binary=False, user_email='gwiktoro@camk.edu.pl', partition=None, dry_run=False, wait=False, **kwargs):
+
+
+    logger.debug(f"Unknown arguments to moccarun(): {kwargs}")
+
+    run_path = Path(run_path).absolute()
+    logger.debug(f"{run_path=}")
 
     # clean directory except nbody, moccaini and mocca.slurm files
     logger.info("Cleaning the current directory")
-    files_to_keep = ['mocca.ini', 'mocca.slurm', 'single_nbody.dat', 'binary_nbody.dat']
+    files_to_keep = ['single_nbody.dat', 'binary_nbody.dat', 'mocca.slurm', 'mocca.ini']  # mocca.ini MUST be last on this list
     if keep_mocca_binary:
         files_to_keep.append('mocca')
 
 #    tmp = '\|'.join(files_to_keep)
 #    cmd = rf"shopt -s extglob; rm -rf !\({tmp}\)"
-    cmd = f"mkdir -p .moccarun_tmp && mv {' '.join(files_to_keep)} .moccarun_tmp && rm -rf * && cp .moccarun_tmp/* . && rm -rf .moccarun_tmp"
+    cmd = f"(cd {run_path} && mkdir -p .moccarun_tmp && for file in {' '.join(files_to_keep)}; do [ -e $file ] && mv $file .moccarun_tmp; done && rm -rf * && mv .moccarun_tmp/* . && rmdir .moccarun_tmp)"
     logger.debug(f"{cmd=}")
     subprocess.run(cmd, shell=True, executable='/bin/bash')
 
-    current_dir = Path(path).absolute()
-    logger.debug(f"{current_dir=}")
+    assert (run_path / 'mocca.ini').is_file(), "mocca.ini missing!"
+    
+
+
     if not keep_mocca_binary:
 
         logger.info("Copying mocca binary")
@@ -47,7 +55,7 @@ def moccarun(path='.', mocca_binary_path=None, keep_mocca_binary=False, user_ema
         else:
             
             # localizing and coping mocca binary
-            parent_dir = current_dir.parent
+            parent_dir = run_path.parent
             while len(parent_dir.parents)>0:
                 if parent_dir.stem == 'src' and (parent_dir / 'mocca').is_file():
                     mocca_binary_path = parent_dir / 'mocca'
@@ -58,14 +66,15 @@ def moccarun(path='.', mocca_binary_path=None, keep_mocca_binary=False, user_ema
                 exit(1)
 
         logger.debug(f"{mocca_binary_path=}")
-        #(current_dir / 'mocca').write_bytes((parent_dir / 'mocca').read_bytes())
-        cmd = f"cp {parent_dir / 'mocca'} {current_dir / 'mocca'}"
+        #(run_path / 'mocca').write_bytes((parent_dir / 'mocca').read_bytes())
+        cmd = f"cp {parent_dir / 'mocca'} {run_path / 'mocca'}"
         logger.debug(f"{cmd=}")
         subprocess.run(cmd, shell=True)
         
     logger.info("Updating slurm script")
+    assert (run_path / 'mocca.slurm').is_file(), "mocca.slurm missing!"
     sed_cmd_l = [
-        f"s/#SBATCH -J .*/#SBATCH -J {current_dir.stem}/",
+        f"s/#SBATCH -J .*/#SBATCH -J {run_path.stem}/",
         f"s/#SBATCH --mail-user=.*/#SBATCH --mail-user={user_email}/"
         ]
     if partition is not None:
@@ -80,12 +89,12 @@ def moccarun(path='.', mocca_binary_path=None, keep_mocca_binary=False, user_ema
             exit(1)
     
     sed_cmd = ';'.join(sed_cmd_l)
-    cmd = f'sed -i "{sed_cmd}" {current_dir/"mocca.slurm"}'
+    cmd = f'sed -i "{sed_cmd}" {run_path/"mocca.slurm"}'
     logger.debug(f"{cmd=}")
     subprocess.run(cmd, shell=True)
 
     if not dry_run:
-        cmd = f"sbatch {'--wait' if wait else ''} mocca.slurm"
+        cmd = f"(cd {run_path} && sbatch {'--wait' if wait else ''} mocca.slurm)"
         logger.debug(f"{cmd=}")
         subprocess.run(cmd, shell=True)
 
@@ -99,13 +108,14 @@ def parse_args():
             """)
 
     # Add the script_path argument
-    parser.add_argument('--path', type=Path, default='.', help='path to dirrectory with mocca.ini and mocca.slurm')
+    parser.add_argument('path', type=Path, default='.', nargs='?', const='.', help='path to dirrectory with mocca.ini and mocca.slurm')
     parser.add_argument('--mocca-binary-path', type=Path, default=None, help='path to mocca binary. If not provided parent directories would be searched')
     parser.add_argument('--user-email', type=str, default='gwiktoro@camk.edu.pl', help='path to dirrectory with mocca.ini')
     parser.add_argument('--partition', type=str, choices=['short','long'], default=None, help='path to dirrectory with mocca.ini')
     parser.add_argument('--dry-run', action='store_true', help='path to dirrectory with mocca.ini')
     parser.add_argument('--logLevel', action='store', choices=['DEBUG','INFO','WARNING','ERROR'], default='WARNING')
     parser.add_argument('--keep-mocca-binary', action='store_true', help='path to dirrectory with mocca.ini')
+    parser.add_argument('--wait', action='store_true', help='wait for simulation to finish (e.g. when used in a pipe)')
 
     # Parse the command-line arguments
     args = parser.parse_args()
@@ -116,11 +126,12 @@ def main():
 
     args = parse_args()
 
-    logger.setLevel(args.logLevel)
+    logger.parent.setLevel(args.logLevel)
     logger.debug(f"{args=}")
+    del args.logLevel
 
     # Call the function to execute the bash script
-    moccarun(path=args.path, mocca_binary_path=args.mocca_binary_path, keep_mocca_binary=args.keep_mocca_binary, user_email=args.user_email, partition=args.partition, dry_run=args.dry_run)
+    moccarun(**vars(args))
 
 if __name__ == '__main__':
     main()
--- scripts/moccatest.py
+++ scripts/moccatest.py
@@ -11,7 +11,7 @@ from datetime import datetime
 
 import logging
 logging.basicConfig(format='%(asctime)s|%(levelname)s|%(name)s|%(funcName)s|%(lineno)s|%(message)s')
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
 
 #from . import moccarun
 from moccarun import moccarun
@@ -21,20 +21,23 @@ def myrun(cmd, **kwargs):
     logger.debug(f"{cmd=}")
     subprocess.run(cmd, shell=True, **kwargs)
 
-def moccatest(test_path, make_path=None, commit=None, ref_path=None, set_moccaini=None, dry_run=False, do_snapshot_test=None, moccarun_kwargs=dict()):
+def moccatest(run_path, make_path=None, commit=None, ref_path=None, set_moccaini=None, dry_run=False, do_snapshot_test=None, moccarun_kwargs=dict(), **kwargs):
     # Execute the bash script using subprocess module
+    assert commit is None or (commit is not None and make_path is not None), "make_path has to be provided together with commit"
+
+    logger.debug(f"Unknown arguments to moccatest(): {kwargs}")
 
     cmd_l = []  # list of commands
 
-    test_path = Path(test_path)
-    #cmd_l.append(f"mkdir -p {test_path}")
-    myrun(f"mkdir -p {test_path}")
+    run_path = Path(run_path)
+    #cmd_l.append(f"mkdir -p {run_path}")
+    myrun(f"mkdir -p {run_path}")
 
     if make_path is not None:
         cmd = f"(cd {make_path} && "
         if commit is not None:
             cmd += f"git checkout -f {commit} && "
-        cmd += "make clean && make debug > .make_debug.log"
+        cmd += "make clean && make debug > .make_debug.log)"
         myrun(cmd)
 #    if compile:
 #        cmd_l.append(f"(cd {src_path} && make clean && make debug > /dev/null)")
@@ -47,37 +50,37 @@ def moccatest(test_path, make_path=None, commit=None, ref_path=None, set_moccain
 #    if test_name is None:
 #        test_name = datetime.now().strftime("%y%m%d%H%M%S")
     
-#    test_path = runs_dir / test_name
-    #test_name = test_path.stem
+#    run_path = runs_dir / test_name
+    #test_name = run_path.stem
     
     #if commit is not None:
     #    test_name += f"_{commit}"
 
     
     if ref_path is not None:
-        myrun(f"cp {ref_path}/{{mocca.ini,mocca.slurm,*_nbody.dat}} {test_path}")
+        myrun(f"cp {ref_path}/{{mocca.ini,mocca.slurm,*_nbody.dat}} {run_path}")
         # only change the job name if we copy mocca.slurm from ref_path
-#        cmd_l.append(f'sed -i "s/#SBATCH -J .*/#SBATCH -J {test_name}/" {test_path}/mocca.slurm')
-#        cmd_l.append(f'sed -i "s/#SBATCH --mail-user=.*/#SBATCH --mail-user={user_email}/" {test_path}/mocca.slurm')
+#        cmd_l.append(f'sed -i "s/#SBATCH -J .*/#SBATCH -J {test_name}/" {run_path}/mocca.slurm')
+#        cmd_l.append(f'sed -i "s/#SBATCH --mail-user=.*/#SBATCH --mail-user={user_email}/" {run_path}/mocca.slurm')
     
     if set_moccaini is not None:
         sed_cmd_l = []
         for param, value in [s.split('=') for s in set_moccaini.replace(' ','').split(',')]:
             sed_cmd_l.append(f"s/{param}\s*=\s*.*/{param} = {value}/")
         sed_cmd = ';'.join(sed_cmd_l)
-        myrun('sed -i "{sed_cmd}" {current_dir/"mocca.slurm"}')
+        myrun(f'sed -i "{sed_cmd}" {run_path/"mocca.ini"}')
 #    if run:
 #        if do_snapshot_test is not None:
-#            cmd_l.append(f'(cd {test_path} && moccarun --wait)')
-#            cmd_l.append(f'test_snapshots.py {do_snapshot_test} {test_path / "snapshot.dat"}')
+#            cmd_l.append(f'(cd {run_path} && moccarun --wait)')
+#            cmd_l.append(f'test_snapshots.py {do_snapshot_test} {run_path / "snapshot.dat"}')
 #        else:
-#            cmd_l.append(f'(cd {test_path} && moccarun)')
+#            cmd_l.append(f'(cd {run_path} && moccarun)')
 
     #cmd = ' && '.join(cmd_l)
     if not dry_run:
         #print(cmd)
         #exit(0)
-        moccarun(path=test_path, **moccarun_kwargs)
+        moccarun(run_path=run_path, **moccarun_kwargs)
 
 def parse_args():
 
@@ -88,14 +91,15 @@ def parse_args():
             """)
 
     # Add the script_path argument
-    parser.add_argument('test_path', type=Path, help='path to the test directory')
+    parser.add_argument('run_path', type=Path, help='path to the test directory')
     parser.add_argument('--make-path', type=Path, help="Do the code compilation before running the test")
     parser.add_argument('--commit', type=str, default=None, help="Commit for the code to test (will affect the code directory!")
     parser.add_argument('--ref-path', type=str, default=None, help='Directory with reference files (mocca.ini, mocca.slurm, *nbody.dat)')
     parser.add_argument('--set-moccaini', type=str, default=None, help='comma separated list of parameter=value to be set in mocca.ini')
     parser.add_argument('--do-snapshot-test', action='store', type=Path, default=None, help="snapshot.dat path to which the results will be compared")
     parser.add_argument('--dry-run', action='store_true', help="Just print the commands and do nothing")
-    parser.add_argument('--moccarun-kwargs', type=json.loads, help="arguments to moccarun as a json string")
+    parser.add_argument('--moccarun-kwargs', type=json.loads, default={}, help="arguments to moccarun as a json string")
+    parser.add_argument('--logLevel', action='store', choices=['DEBUG','INFO','WARNING','ERROR'], default='WARNING')
 
     # Parse the command-line arguments
     args = parser.parse_args()
@@ -106,10 +110,12 @@ def main():
 
     args = parse_args()
 
-    print(args)
+    logger.parent.setLevel(args.logLevel)  # set to parent so it's visible in submodules
+    logger.debug(args)
+    del args.logLevel  # not needed in other functions
 
     # Call the function to execute the bash script
-    #moccatest(args.test_path, src_path=args.src_path, ref_path=args.ref_path, user_email=args.user_email, commit=args.commit, compile=args.compile, set_moccaini=args.set_moccaini, do_snapshot_test=args.do_snapshot_test, dry_run=args.dry_run, run=not args.do_not_run)
+    #moccatest(args.run_path, src_path=args.src_path, ref_path=args.ref_path, user_email=args.user_email, commit=args.commit, compile=args.compile, set_moccaini=args.set_moccaini, do_snapshot_test=args.do_snapshot_test, dry_run=args.dry_run, run=not args.do_not_run)
     moccatest(**vars(args))
     
 if __name__ == '__main__':
